73c73b52352fb90b1eff10b0c416de2bc0baff4b
Jung in Lee <43091713+herjn@users.noreply.github.com>
2020-03-14 01:19:25 +0900

    create.sql
    create sql statement

---
+++ create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `report` (
+  `idx` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `writer` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `administer` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adcompany1` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sn1` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sn2` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sn3` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tag` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `regdate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `moddate` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idx`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
