7e76658cef98b6817820ffba5601cab0f0cc941b
Jung in Lee <43091713+herjn@users.noreply.github.com>
2020-03-24 00:24:09 +0900

    Update change.sql

--- change.sql
+++ change.sql
@@ -12,6 +12,6 @@ CREATE TABLE `report` (
   `clientsym` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `repairsym` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `comment` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `immage` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`idx`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
